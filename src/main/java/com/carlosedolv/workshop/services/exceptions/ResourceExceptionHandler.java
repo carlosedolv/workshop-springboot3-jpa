31adb091edfb17cfd56497956cb8e36e3245946f
Carlos <carlosmcof@gmail.com>
2025-09-10 16:07:32 -0300

    Service layer: Exception handling -> User.delete, User.update

--- src/main/java/com/carlosedolv/workshop/services/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/carlosedolv/workshop/services/exceptions/ResourceExceptionHandler.java
@@ -19,4 +19,12 @@ public class ResourceExceptionHandler {
 		StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(standardError);
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> dbException(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		String error = "Database error.";
+		StandardError standardError = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(standardError);
+	}
 }
